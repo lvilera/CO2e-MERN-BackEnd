768ed3a195a780c4ee35df10959ec0abc3cd108d
A4-AryanArshad <2021cs613@student.uet.edu.pk>
2025-07-17 13:04:19 +0500

    Your meaningful commit message

--- api/index.js
+++ api/index.js
@@ -20,7 +20,7 @@ const app = express();
 
 // CORS middleware - must be before any routes or express.json()
 app.use(cors({
-   origin: 'http://localhost:3000',
+   origin: 'https://e-frontend-wf3o.vercel.app',
    //origin: '*',
    credentials: true
  }));
