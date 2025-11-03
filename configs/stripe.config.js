14084a9fd1538644f7b21967404a668d4c00806d
Belayet Riad <belayetriadbd@gmail.com>
2025-11-03 15:41:20 -0800

    Update stripe

--- configs/stripe.config.js
+++ configs/stripe.config.js
@@ -1,4 +1,4 @@
 const Stripe = require('stripe');
-const stripe = Stripe(process.env.STRIPE_SECRET_KEY || 'sk_test_51Rj1BnQqlrTjfVZHQxOrodCldv1ac5l72oVnAVgOdyZ7Z1bEBwIStbD012CS4ZKG9gVSpqw701l5yz4NVlWKQkR1009JlSK0Il');
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 
 module.exports = stripe;
