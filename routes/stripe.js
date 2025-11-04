59400769e731731f10046c01d587152254f30d2d
Belayet Riad <belayetriadbd@gmail.com>
2025-11-03 16:50:40 -0800

    update env

--- routes/stripe.js
+++ routes/stripe.js
@@ -61,7 +61,7 @@ router.post('/create-checkout-session', authMiddleware.authenticateRequest, asyn
     let stripeCustomerId = await userService.fetchUserStripeCustomerId(userId);
     if (!stripeCustomerId) {
       const user = await userService.fetchUser(userId);
-      stripeCustomerId = await stripeService.createCustomer(user.name, user.email);
+      stripeCustomerId = await stripeService.createCustomer(user.firstName + '' + user.lastName, user.email);
       await userService.updateUser(userId, { stripeCustomerId });
     }
     const sessionURL = await stripeService.createCheckoutSession(priceId, stripeCustomerId, CLIENT_URL, mode, type, quantity);
