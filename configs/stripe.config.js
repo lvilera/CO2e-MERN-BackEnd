8274401b56fe7aa2a524c606bcc601a332f88a34
Belayet Riad <belayetriadbd@gmail.com>
2025-11-03 16:20:39 -0800

    update env

--- configs/stripe.config.js
+++ configs/stripe.config.js
@@ -1,16 +1,4 @@
-const path = require('path');
-const dotenvPath = path.resolve(__dirname, '../.env');
-console.log('Loading .env from:', dotenvPath);
-
-require('dotenv').config({ path: dotenvPath });
-
-//console.log('STRIPE_SECRET_KEY:', process.env.STRIPE_SECRET_KEY ? 'Loaded ✅' : 'Not found ❌');
-
 const Stripe = require('stripe');
-const { STRIPE_SECRET_KEY } = process.env;
+const stripe = Stripe(process.env.STRIPE_SECRET_KEY || 'sk_test_51Rj1BnQqlrTjfVZHQxOrodCldv1ac5l72oVnAVgOdyZ7Z1bEBwIStbD012CS4ZKG9gVSpqw701l5yz4NVlWKQkR1009JlSK0Il');
 
-if (!STRIPE_SECRET_KEY) {
-  throw new Error('Missing STRIPE_SECRET_KEY');
-}
-
-module.exports = new Stripe(STRIPE_SECRET_KEY, { apiVersion: '2024-06-20' });
+module.exports = stripe;
